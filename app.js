7e7f6ae2af9352c5abe083f6ba1124baa8f4d88b
WolfDillinger <66114913+WolfDillinger@users.noreply.github.com>
2024-05-17 14:28:21 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const authReouter = require("./routes/auth");
+const cors = require('cors');
 
 const PORT = 2030;
 
 const DBC = "mongodb+srv://jcpofficialjo:mg9kxsNz64P1RlH4@cluster0.ugf8uor.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 const app = express();
+
+const corsOptions = {
+  origin: 'https://batayneh-store.com',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Add the allowed methods
+  allowedHeaders: ['Content-Type', 'Authorization', 'access-control-allow-methods'], // Include 'access-control-allow-methods'
+};
+
+app.use(cors(corsOptions));
 
 app.use(express.json());
 
